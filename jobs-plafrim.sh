ed1f4308d4a0b478924de0d42991f6ae1d6e269b
Berenger Bramas <Berenger.Bramas@inria.fr>
2023-07-12 16:34:59 +0200

    add avx results

--- jobs-plafrim.sh
+++ jobs-plafrim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH --job-name=spmv
-#SBATCH --time=24:00:00
+#SBATCH --time=5:00:00
 #SBATCH --exclusive
 #SBATCH -C "bora&intel&cascadelake"
 #SBATCH --ntasks-per-node=1
