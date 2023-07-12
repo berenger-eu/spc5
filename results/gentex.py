01cc751cd2364797f1f2d3158a928d2c14dc9798
Berenger Bramas <Berenger.Bramas@inria.fr>
2023-07-12 18:29:47 +0200

    update script'

--- results/gentex.py
+++ results/gentex.py
@@ -105,94 +105,104 @@ for hsum in hsumtypes:
         res[idx] = ''
         
 ##############################################################
-print('====================================================')
-print('====================================================')
-print('compare hsum or not')
-print('====================================================')
-print('====================================================')
-# Get the number of rows and calculate the number of subplots needed
-num_rows = 0
+facto_column_name = 'facto_column_name'
+if facto_column_name in df.columns:
+    print(f"Column '{facto_column_name}' exists.")
+    print('====================================================')
+    print('====================================================')
+    print('compare facto or not')
+    print('====================================================')
+    print('====================================================')
+    
+    for hsum in hsumtypes:
+        print('====================================================')
+        print('====================================================')
+        print(hsum)
+        print('====================================================')
+        print('====================================================')
+        # Get the number of rows and calculate the number of subplots needed
+        num_rows = 0
 
-nb_mat_per_row=12
+        nb_mat_per_row=12
 
-labels=['scalar', '1rVc', '2rVc', '4rVc', '8rVc']
-res=['', '', '', '', '']
-avgf64=[0,0,0,0,0]
-avgf32=[0,0,0,0,0]
+        labels=['scalar', '1rVc', '2rVc', '4rVc', '8rVc']
+        res=['', '', '', '', '']
+        avgf64=[0,0,0,0,0]
+        avgf32=[0,0,0,0,0]
 
-ticks=''
+        ticks=''
 
-# Iterate over each row in the DataFrame
-for i, row in df.iterrows():
-    # print(str(i) + " " + str(row))
-    # Get the data for the current row
-    matrixname = row['matrixname']
-    if 'CO' in matrixname or 'dense' in matrixname or 'nd6k' in matrixname:
-        matrixname=matrixname.replace('_', ' ')
-        types = row['type']
-        hsum = 'multi-sum' if row['hsum'] == 'withhsum' else 'reduction'
-        
-        matrixnamewithtype = matrixname + ' ' + hsum + ' (f64)' if types == 'double' else matrixname + ' ' + hsum + ' (f32)'
-
-        values = row[['scalar', '1rVc', '2rVc', '4rVc', '8rVc']].astype(float)
-            
-        # print(matrixname)
-        # print(str(values))
-        # print(str(values.index))
-        
-        ticks += matrixnamewithtype if ticks == '' else ',' + matrixnamewithtype
-        
-        for idx,lb in enumerate(labels):
-            val=values[idx]
-            if types == 'double':
-                avgf64[idx] += val
-            else:
-                avgf32[idx] += val
-            if idx == 0:
-                res[idx] += f' ({matrixnamewithtype}, {val}) '
-            else:
-                speedup=values[idx]/values[0]
-                res[idx]+= f' ({matrixnamewithtype}, {val})[{speedup:.1f}] '
+        # Iterate over each row in the DataFrame
+        for i, row in df.iterrows():
+            # print(str(i) + " " + str(row))
+            # Get the data for the current row
+            matrixname = row['matrixname']
+            if 'CO' in matrixname or 'dense' in matrixname or 'nd6k' in matrixname:
+                matrixname=matrixname.replace('_', ' ')
+                types = row['type']
+                facto = 'full x load' if row[facto_column_name] == 'Facto' else 'partial x load'
                 
-        if (num_rows+1)%nb_mat_per_row == 0:
-            print('===================================')
-            print(ticks)
-            ticks=''
-            for idx,lb in enumerate(labels):
-                print('\\addplot[draw=color_' + str(idx) +'!65!black, fill=color_' + str(idx) +'!30!white] coordinates { ' + res[idx] + ' };')
-                res[idx] = ''
-        num_rows = num_rows + 1
+                matrixnamewithtype = matrixname + ' ' + facto + ' (f64)' if types == 'double' else matrixname + ' ' + facto + ' (f32)'
 
-print('===================================')
-print(ticks)
-ticks=''
-for idx,lb in enumerate(labels):
-    print('\\addplot[draw=color_' + str(idx) +'!65!black, fill=color_' + str(idx) +'!30!white] coordinates { ' + res[idx] + ' };')
-    res[idx] = ''
+                values = row[['scalar', '1rVc', '2rVc', '4rVc', '8rVc']].astype(float)
+                    
+                # print(matrixname)
+                # print(str(values))
+                # print(str(values.index))
+                
+                ticks += matrixnamewithtype if ticks == '' else ',' + matrixnamewithtype
+                
+                for idx,lb in enumerate(labels):
+                    val=values[idx]
+                    if types == 'double':
+                        avgf64[idx] += val
+                    else:
+                        avgf32[idx] += val
+                    if idx == 0:
+                        res[idx] += f' ({matrixnamewithtype}, {val}) '
+                    else:
+                        speedup=values[idx]/values[0]
+                        res[idx]+= f' ({matrixnamewithtype}, {val})[{speedup:.1f}] '
+                        
+                if (num_rows+1)%nb_mat_per_row == 0:
+                    print('===================================')
+                    print(ticks)
+                    ticks=''
+                    for idx,lb in enumerate(labels):
+                        print('\\addplot[draw=color_' + str(idx) +'!65!black, fill=color_' + str(idx) +'!30!white] coordinates { ' + res[idx] + ' };')
+                        res[idx] = ''
+                num_rows = num_rows + 1
 
-#############################   
-ticks+= ',average (f64), average (f32)'
-for idx,lb in enumerate(labels):
-    val0=avgf64[0]/(num_rows/2)
-    val=avgf64[idx]/(num_rows/2)
-    if idx == 0:
-        res[idx] += f' (average (f64), {val}) '
-    else:
-        speedup=val/val0
-        res[idx]+= f' (average (f64), {val})[{speedup:.1f}] '
-for idx,lb in enumerate(labels):
-    val0=avgf32[0]/(num_rows/2)
-    val=avgf32[idx]/(num_rows/2)
-    if idx == 0:
-        res[idx] += f' (average (f32), {val}) '
-    else:
-        speedup=val/val0
-        res[idx]+= f' (average (f32), {val})[{speedup:.1f}] '        
+        print('===================================')
+        print(ticks)
+        ticks=''
+        for idx,lb in enumerate(labels):
+            print('\\addplot[draw=color_' + str(idx) +'!65!black, fill=color_' + str(idx) +'!30!white] coordinates { ' + res[idx] + ' };')
+            res[idx] = ''
 
-print('===================================')
+        #############################   
+        ticks+= ',average (f64), average (f32)'
+        for idx,lb in enumerate(labels):
+            val0=avgf64[0]/(num_rows/2)
+            val=avgf64[idx]/(num_rows/2)
+            if idx == 0:
+                res[idx] += f' (average (f64), {val}) '
+            else:
+                speedup=val/val0
+                res[idx]+= f' (average (f64), {val})[{speedup:.1f}] '
+        for idx,lb in enumerate(labels):
+            val0=avgf32[0]/(num_rows/2)
+            val=avgf32[idx]/(num_rows/2)
+            if idx == 0:
+                res[idx] += f' (average (f32), {val}) '
+            else:
+                speedup=val/val0
+                res[idx]+= f' (average (f32), {val})[{speedup:.1f}] '        
 
-print(ticks)
-ticks=''
-for idx,lb in enumerate(labels):
-    print('\\addplot[draw=color_' + str(idx) +'!65!black, fill=color_' + str(idx) +'!30!white] coordinates { ' + res[idx] + ' };')
-    res[idx] = ''
+        print('===================================')
+
+        print(ticks)
+        ticks=''
+        for idx,lb in enumerate(labels):
+            print('\\addplot[draw=color_' + str(idx) +'!65!black, fill=color_' + str(idx) +'!30!white] coordinates { ' + res[idx] + ' };')
+            res[idx] = ''
